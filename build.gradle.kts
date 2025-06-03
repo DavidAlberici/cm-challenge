bcbc4d06ec12336134b9c3ffe738c17102d87019
David Alberici <davidalberici3200@gmail.com>
2025-06-03 09:36:47 +0200

    Added lombok and mockito dependencies (should have done it 2 commits ago)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,19 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    // lombok
     compileOnly("org.projectlombok:lombok:1.18.38")
     annotationProcessor("org.projectlombok:lombok:1.18.38")
 
     testCompileOnly("org.projectlombok:lombok:1.18.38")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    // jackson mapper
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+
+    // mockito
+    testImplementation("org.mockito:mockito-core:2.1.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.18.0")
 }
 
 tasks.test {
